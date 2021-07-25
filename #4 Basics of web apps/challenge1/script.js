1b5a5cef4809d4680224ed6b8e44d8a3f9011e1b
totman <totman@totmans-MacBook-Pro.local>
2021-07-25 22:25:59 +0900

    #4 created

---
+++ #4 Basics of web apps/challenge1/script.js
@@ -0,0 +1,14 @@
+
+// console.log(document);
+
+window.onload = function(){
+  // console.log(document.getElementById('list').firstChild);
+  // document.getElementById('list').firstChild.setAttribute('style', 'color: red');
+  document.getElementById('list').firstChild.setAttribute('style', 'margin-top: 50px');
+  document.getElementById('title').setAttribute('style', 'color: orange');
+}
+
+
+var addTask = function(){
+  console.log('ボタンが押されたぞ！');
+}
---
+++ #4 Basics of web apps/lesson/script.js
@@ -0,0 +1,12 @@
+
+// console.log(document);
+
+window.onload = function(){
+  // console.log(document.getElementById('list').firstChild);
+  // document.getElementById('list').firstChild.setAttribute('style', 'color: red');
+}
+
+
+var addTask = function(){
+  console.log('ボタンが押されたぞ！');
+}
