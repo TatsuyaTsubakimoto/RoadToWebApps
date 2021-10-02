9d02785f8de35342b7e20889f541245f037502d3
totman <totman@totmans-MacBook-Pro.local>
2021-10-02 11:18:53 +0900

    updates for Tonomori ver. some challenges are different from textbook

--- #5 Basics of web apps/lesson/script.js
+++ #5 Basics of web apps/lesson/script.js
@@ -1,17 +1,30 @@
+// console.log(document);
 
-window.onload = function(){
-  // console.log(document.getElementById('list').firstChild.firstChild);
-  // document.getElementById('list').firstChild.firstChild.setAttribute('color', 'red');
-}
+$(function() {
+  // テキストを取得
+  console.log($('h1').text());
+  // テキストを変更
+  $('h1').text('new text');
+  // 色を変更
+  $('h1').css('color', 'red');
+  // 複数の要素を取得
+  console.log($('li').text());
+  // 複数の要素から一つを選択して取得
+  console.log($('li').eq(0).text());
+  // 要素を追加
+  $('ul').append('<li>jQueryで追加されたタスク</li>');
+});
 
-
-let addTask = function(){
-  let liNode = document.createElement('li');
-  let textNode = document.createTextNode(document.getElementById('input').value);
-  li.addEventListener('click', () => setDone(liNode));
-  document.getElementById('list').appendChild(liNode).appendChild(textNode);
-}
 
 let setDone = function(element){
-  console.log('タスクが押されたぞ！');
+  console.log($('#input').val());
+  console.log('ボタンが押されたぞ！');
+}
+
+// タスクを追加
+let addTask = function(){
+  // インプットの文字を取得
+  let text = $('input').val();
+  // 要素を追加
+  $('ul').append('<li>' + text + '</li>');
 }
--- #6 Basics of web apps/lesson/script.js
+++ #6 Basics of web apps/lesson/script.js
@@ -1,8 +1,21 @@
-$(function() {
-  // $('h1').css('color', 'red');
-  // $('h1').text('new text');
-});
+// console.log(document);
 
+// $(function() {
+//   // テキストを取得
+//   console.log($('h1').text());
+//   // テキストを変更
+//   $('h1').text('new text');
+//   // 色を変更
+//   $('h1').css('color', 'red');
+//   // 複数の要素を取得
+//   console.log($('li').text());
+//   // 複数の要素から一つを選択して取得
+//   console.log($('li').eq(0).text());
+//   // 要素を追加
+//   $('ul').append('<li>jQueryで追加されたタスク</li>');
+// });
+
+// タスクを追加
 let addTask = function(){
   // インプットの文字を取得
   let text = $('input').val();
